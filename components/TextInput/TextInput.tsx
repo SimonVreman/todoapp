519c4cd745ff9cc3fc1da5e1b4c338296f0d6caa
simon <info@simonvreman.nl>
2023-04-21 08:42:26 +0200

    feat: setup todo form

--- components/TextInput/TextInput.tsx
+++
@@ -1,9 +0,0 @@
-import Input from "@/components/Input/Input"
-
-export default function TextInput({ label, content }: { label: string; content: string }) {
-  return (
-    <Input label={label}>
-      <input className={"rounded-md border border-gray-300 p-1 w-full"} value={content} />
-    </Input>
-  )
-}
--- components/TodoForm/TodoForm.tsx
+++ components/TodoForm/TodoForm.tsx
@@ -1,9 +1,18 @@
+"use client"
+
 import { NonPersistedTodo, Todo } from "@/types/todo"
-import TextInput from "@/components/TextInput/TextInput"
-import TextAreaInput from "@/components/TextAreaInput/TextAreaInput"
-import SliderInput from "@/components/SliderInput/SliderInput"
+import React from "react"
+import { useForm } from "react-hook-form"
+import classNames from "classnames"
 
 export default function TodoForm({ oldTodo }: { oldTodo?: Todo }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+  const onSubmit = (data) => {}
+
   const todo: NonPersistedTodo = {
     name: oldTodo?.name ?? "",
     description: oldTodo?.description ?? "",
@@ -12,17 +21,59 @@ export default function TodoForm({ oldTodo }: { oldTodo?: Todo }) {
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className={"mb-2"}>
-        <TextInput label={"Name"} content={todo.name} />
+        <label
+          className={classNames("text-gray-600 mb-1", {
+            "text-red-600": errors.name,
+          })}
+        >
+          Name {errors.name?.message}
+        </label>
+        <input
+          className={"rounded-md border border-gray-300 p-1 w-full"}
+          defaultValue={todo.name}
+          {...register("name", { required: true, minLength: 3, maxLength: 40 })}
+          aria-invalid={errors.name ? "true" : "false"}
+        />
       </div>
 
       <div className={"mb-2"}>
-        <TextAreaInput className={"mb-2"} label={"Description"} content={todo.description} />
+        <label
+          className={classNames("text-gray-600 mb-1", {
+            "text-red-600": errors.description,
+          })}
+        >
+          Description
+        </label>
+        <textarea
+          className={"rounded-md border border-gray-300 p-1 w-full resize-none"}
+          rows={3}
+          defaultValue={todo.description}
+          aria-invalid={errors.description ? "true" : "false"}
+          {...register("description", { required: false, maxLength: 250 })}
+        ></textarea>
       </div>
 
       <div className={"mb-2"}>
-        <SliderInput className={"mb-2"} label={"Priority"} value={todo.priority} min={1} max={3} step={1} />
+        <label
+          className={classNames("text-gray-600 mb-1", {
+            "text-red-600": errors.priority,
+          })}
+        >
+          Priority
+        </label>
+        <input
+          className={"w-full"}
+          type={"range"}
+          step={1}
+          {...register("priority", { required: true, min: 1, max: 3 })}
+          aria-invalid={errors.priority ? "true" : "false"}
+        />
+      </div>
+
+      <div className={"mb-2"}>
+        <button className={"bg-blue-500 text-white rounded-md px-3 py-1"}>Save</button>
       </div>
     </form>
   )
