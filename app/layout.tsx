d54d7711127de59bc00d825eb980098b5483eab4
simon <info@simonvreman.nl>
2023-06-12 22:01:50 +0200

    feat: improve mobile ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,18 +12,20 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={"w-screen h-screen flex justify-center p-10"}>
+      <body className={"w-screen h-screen flex justify-center sm:p-10"}>
         <div
-          className={"container max-w-screen-lg min-h-full flex flex-col sm:flex-row rounded-lg border border-gray-300"}
+          className={
+            "container max-w-screen-lg min-h-full flex flex-col sm:flex-row sm:rounded-lg border border-gray-300"
+          }
         >
-          <nav className={"w-full h-52 sm:w-52 sm:h-full bg-gray-200 rounded-t-lg sm:rounded-r-none sm:rounded-l-lg"}>
+          <nav className={"w-full h-64 sm:w-52 sm:h-full bg-gray-200 sm:rounded-r-none sm:rounded-l-lg"}>
             <ErrorBoundary fallback={<NavigationError />}>
               <Navigation />
             </ErrorBoundary>
           </nav>
           <main
             className={
-              "flex-grow bg-gray-100 border-t sm:border-t-0 sm:border-l border-gray-300 p-4 rounded-b-lg sm:rounded-l-none sm:rounded-r-lg"
+              "flex-grow bg-gray-100 border-t sm:border-t-0 sm:border-l border-gray-300 p-4 sm:rounded-l-none sm:rounded-r-lg overflow-scroll"
             }
           >
             {children}
