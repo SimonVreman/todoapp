95289340fe0f1965e30c9ee97dc77ea81a3507da
simon <info@simonvreman.nl>
2023-06-12 22:05:32 +0200

    feat: improve mobile ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={"w-screen h-screen flex justify-center sm:p-10"}>
         <div
           className={
-            "container max-w-screen-lg min-h-full flex flex-col sm:flex-row sm:rounded-lg border border-gray-300"
+            "container max-w-screen-lg min-h-full flex flex-col sm:flex-row sm:rounded-lg sm:border border-gray-300"
           }
         >
           <nav className={"w-full h-64 sm:w-52 sm:h-full bg-gray-200 sm:rounded-r-none sm:rounded-l-lg"}>
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </nav>
           <main
             className={
-              "flex-grow bg-gray-100 border-t sm:border-t-0 sm:border-l border-gray-300 p-4 sm:rounded-l-none sm:rounded-r-lg overflow-scroll"
+              "flex-grow bg-gray-100 overflow-scroll border-t sm:border-t-0 sm:border-l border-gray-300 p-4 sm:rounded-l-none sm:rounded-r-lg"
             }
           >
             {children}
