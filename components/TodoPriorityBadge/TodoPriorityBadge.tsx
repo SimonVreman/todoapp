05fc7c721b006a4342648179940a2e264dc9b91b
simon <info@simonvreman.nl>
2023-06-12 18:03:05 +0200

    feat: todo priority badge

---
+++ components/TodoPriorityBadge/TodoPriorityBadge.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import classNames from "classnames"
+
+export default function TodoPriorityBadge({ priority }: { priority: number }) {
+  return (
+    <span
+      className={classNames("mr-2 flex h-6 w-6 items-center justify-center rounded-md", {
+        "bg-green-200 text-green-900": +priority === 1,
+        "bg-yellow-200 text-yellow-900": +priority === 2,
+        "bg-red-200 text-red-900": +priority === 3,
+      })}
+    >
+      {priority}
+    </span>
+  )
+}
