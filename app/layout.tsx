6e4d3bca3d1fa502acb6c64eab0d1215ad1ead0a
simon <info@simonvreman.nl>
2023-06-12 17:00:26 +0200

    fix: background overlapping rounded corners in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import "./globals.css"
-import React, { Suspense } from "react"
+import React from "react"
 import Navigation from "@/components/Navigation/Navigation"
 import NavigationError from "@/components/Navigation/NavigationError"
 import ErrorBoundary from "@/components/ErrorBoundary/ErrorBoundary"
 import "react-loading-skeleton/dist/skeleton.css"
-import NavigationLoading from "@/components/Navigation/NavigationLoading"
 
 export const metadata = {
   title: "todo",
@@ -17,12 +16,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <div
           className={"container max-w-screen-lg min-h-full flex flex-col sm:flex-row rounded-lg border border-gray-300"}
         >
-          <nav className={"w-full h-52 sm:w-52 sm:h-full bg-gray-200"}>
+          <nav className={"w-full h-52 sm:w-52 sm:h-full bg-gray-200 rounded-t-lg sm:rounded-r-none sm:rounded-l-lg"}>
             <ErrorBoundary fallback={<NavigationError />}>
               <Navigation />
             </ErrorBoundary>
           </nav>
-          <main className={"flex-grow bg-gray-100 border-t sm:border-t-0 sm:border-l border-gray-300 p-4"}>
+          <main
+            className={
+              "flex-grow bg-gray-100 border-t sm:border-t-0 sm:border-l border-gray-300 p-4 rounded-b-lg sm:rounded-l-none sm:rounded-r-lg"
+            }
+          >
             {children}
           </main>
         </div>
