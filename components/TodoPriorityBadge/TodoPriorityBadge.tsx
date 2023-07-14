9517ea10ddbac400ce2af7517346c3832577702a
simon <info@simonvreman.nl>
2023-07-14 10:24:20 +0200

    Fix priority badge shrinking

--- components/TodoPriorityBadge/TodoPriorityBadge.tsx
+++ components/TodoPriorityBadge/TodoPriorityBadge.tsx
@@ -4,7 +4,7 @@ import classNames from "classnames"
 export default function TodoPriorityBadge({ priority }: { priority: number }) {
   return (
     <span
-      className={classNames("mr-2 flex h-6 w-6 items-center justify-center rounded-md transition-colors", {
+      className={classNames("mr-2 flex h-6 shrink-0 w-6 items-center justify-center rounded-md transition-colors", {
         "bg-green-200 text-green-900": +priority === 1,
         "bg-yellow-200 text-yellow-900": +priority === 2,
         "bg-red-200 text-red-900": +priority === 3,
